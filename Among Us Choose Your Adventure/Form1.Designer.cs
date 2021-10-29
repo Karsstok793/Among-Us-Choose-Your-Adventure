422723bd38ee0515aa5adb8767b702d08dd919a2
P4 <P4@DESKTOP-UA04HP9>
2021-10-29 14:17:54 -0400

    Add project files.

---
+++ Among Us Choose Your Adventure/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Among_Us_Choose_Your_Adventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.QuestionBox = new System.Windows.Forms.Label();
+            this.Option1 = new System.Windows.Forms.Button();
+            this.Option2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.Black;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(128, 81);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(459, 81);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Title Box";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // QuestionBox
+            // 
+            this.QuestionBox.BackColor = System.Drawing.Color.Black;
+            this.QuestionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionBox.ForeColor = System.Drawing.Color.White;
+            this.QuestionBox.Location = new System.Drawing.Point(200, 152);
+            this.QuestionBox.Name = "QuestionBox";
+            this.QuestionBox.Size = new System.Drawing.Size(315, 48);
+            this.QuestionBox.TabIndex = 1;
+            this.QuestionBox.Text = "Question Box";
+            this.QuestionBox.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Option1
+            // 
+            this.Option1.BackColor = System.Drawing.Color.Black;
+            this.Option1.ForeColor = System.Drawing.Color.White;
+            this.Option1.Location = new System.Drawing.Point(132, 241);
+            this.Option1.Name = "Option1";
+            this.Option1.Size = new System.Drawing.Size(94, 38);
+            this.Option1.TabIndex = 2;
+            this.Option1.Text = "Option 1";
+            this.Option1.UseVisualStyleBackColor = false;
+            this.Option1.Click += new System.EventHandler(this.Option1_Click);
+            // 
+            // Option2
+            // 
+            this.Option2.BackColor = System.Drawing.Color.Black;
+            this.Option2.ForeColor = System.Drawing.Color.White;
+            this.Option2.Location = new System.Drawing.Point(487, 241);
+            this.Option2.Name = "Option2";
+            this.Option2.Size = new System.Drawing.Size(94, 38);
+            this.Option2.TabIndex = 3;
+            this.Option2.Text = "Option 2";
+            this.Option2.UseVisualStyleBackColor = false;
+            this.Option2.Click += new System.EventHandler(this.Option2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::Among_Us_Choose_Your_Adventure.Properties.Resources.Start_Scene;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Option2);
+            this.Controls.Add(this.Option1);
+            this.Controls.Add(this.QuestionBox);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label QuestionBox;
+        private System.Windows.Forms.Button Option1;
+        private System.Windows.Forms.Button Option2;
+    }
+}
+
---
+++ Among Us Choose Your Adventure/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Among_Us_Choose_Your_Adventure
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        int chance;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            Title.Text = "You are on the Skeld with 4 others";
+            QuestionBox.Text = "Do you want to be crewmate or impostor";
+            Option1.Text = "impostor";
+            Option2.Text = "crewmate";
+        }
+
+
+
+        private void Option1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                Random randgen= new Random();
+                chance = randgen.Next(1, 11);
+
+                if(chance > 1)
+                {
+                    page = 5;
+                }
+                else
+                {
+                    page = 99;
+                }
+                
+            }
+            else if (page == 3)
+            {
+                page = 6;
+            }
+            else if (page == 4)
+            {
+                page = 14;
+            }
+            else if (page == 5)
+            {
+                page = 10;
+            }
+            else if (page == 6)
+            {
+                page = 18;
+            }
+            else if (page == 7)
+            {
+                Random randgen = new Random();
+                chance = randgen.Next(1, 3);
+                if (chance == 1)
+                {
+                    page = 9;
+                }
+                else
+                {
+                    page = 99;
+                }
+                
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 10)
+            {
+                page = 12;
+            }
+            else if (page == 11)
+            {
+                page = 1;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+            else if (page == 13)
+            {
+                page = 16;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+            else if (page == 15)
+            {
+                page = 1;
+            }
+            else if (page == 16)
+            {
+                page = 1;
+            }
+            else if (page == 17)
+            {
+                page = 1;
+            }
+            else if (page == 18)
+            {
+                page = 1;
+            }
+            else if (page == 19)
+            {
+                page = 1;
+            }
+
+            displayPage();
+
+        }
+        public void displayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    Title.Text = "You are on the Skeld with 4 others";
+                    QuestionBox.Text = "Do you want to be crewmate or impostor";
+                    Option1.Text = "impostor";
+                    Option2.Text = "crewmate";
+                    break;
+                case 2:
+                    Title.Text = "You arrive at electrical, there are two others, green and yellow";
+                    QuestionBox.Text = "Do you want to kill green?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 3:
+                    Title.Text = "You spawn in, you decide to keep red close as an alibi";
+                    QuestionBox.Text = "Do you want to do tasks or go to cameras?";
+                    Option1.Text = "Tasks";
+                    Option2.Text = "Cameras";
+                    break;
+                case 4:
+                    Title.Text = "You fake tasks and wait for the room to clear out";
+                    QuestionBox.Text = "Do you want to vent to medbay or sabotage reactor?";
+                    Option1.Text = "Vent";
+                    Option2.Text = "sabotage";
+                    break;
+                case 5:
+                    Title.Text = "Succesful Kill on green";
+                    QuestionBox.Text = "Do you want to self report?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 6:
+                    Title.Text = "You succesfully do tasks but do not know who the impostor is. A body was found and reported and blue and red are most sus";
+                    QuestionBox.Text = "Do You want to vote out red or blue?";
+                    Option1.Text = "red";
+                    Option2.Text = "blue";
+                    break;
+                case 7:
+                    Title.Text = "You get to cameras safely and you watch for anything sus, you see yellow in cafe";
+                    QuestionBox.Text = "";
+                    Option1.Text = "continue";
+                    Option2.Text = "";
+                    break;
+                case 8:
+                    Title.Text = "The impostor came up behind you while on cams and kills you.";
+                    QuestionBox.Text = "Impostor Wins! Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 9:
+                    Title.Text = "You see red kill blue on camera, and call a meeting. You convince everyone to vote out red";
+                    QuestionBox.Text = "Crewmates win! Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 10:
+                    Title.Text = "You self repot the body and try to act like a crewmate";
+                    QuestionBox.Text = "";
+                    Option1.Text = "continue";
+                    Option2.Text = "";
+                    break;
+                case 11:
+                    Title.Text = "You run away from the body but yellow sees you and then reports the body and gets you voted out.";
+                    QuestionBox.Text = "Crewmates Win, Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 12:
+                    Title.Text = "The crewmate did not believe your lies and you are voted out";
+                    QuestionBox.Text = "Crewmates Win, Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 13:
+                    Title.Text = "You make it past the meeting with everyone skipping due to no evidence.";
+                    QuestionBox.Text = "Do you want to kill yellow?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 14:
+                    Title.Text = "You vent over to medbay but got caught by yellow popping out. Yellow calls meeting and you are out";
+                    QuestionBox.Text = "Crewmates Win...Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 15:
+                    Title.Text = "You sabotage reactor and pretend to go help out but no crewmates made it in time, reactor fails and you win";
+                    QuestionBox.Text = "Impostor wins....Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 16:
+                    Title.Text = "You sneak up and kill yellow and win majority";
+                    QuestionBox.Text = "Impostor wins...Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 17:
+                    Title.Text = "All of the tasks have been completed and the round ends.";
+                    QuestionBox.Text = "Crewmates won from tasks...Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 18:
+                    Title.Text = "You collectively vote out red and win ";
+                    QuestionBox.Text = "Crewmates Win... Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 19:
+                    Title.Text = "Blue gets voted out and impostor wins by majority";
+                    QuestionBox.Text = "Impostor wins... Play again?";
+                    Option1.Text = "yes";
+                    Option2.Text = "no";
+                    break;
+                case 99:
+                    Title.Text = "Thanks for playing!";
+                    QuestionBox.Text = "";
+                    Option1.Visible = false;
+                    Option2.Visible = false;
+                    
+                    
+
+                    Refresh();
+                    Thread.Sleep(2500);
+                    Application.Exit();
+
+                    break;
+                default:
+                    break;
+
+
+
+            }
+        }
+
+        private void Option2_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 3)
+            {
+                page = 7;
+            }
+            else if (page == 4)
+            {
+                page = 15;
+            }
+            else if (page == 5)
+            {
+                page = 11;
+            }
+            else if (page == 6)
+            {
+                page = 19;
+            }
+            else if (page == 7)
+            {
+                page = 9;
+            }
+            else if (page == 8)
+            {
+                page = 99;
+            }
+            else if (page == 9)
+            {
+                page = 99;
+            }
+            else if (page == 10)
+            {
+                page = 13;
+            }
+            else if (page == 11)
+            {
+                page = 99;
+            }
+            else if (page == 12)
+            {
+                page = 99;
+            }
+            else if (page == 13)
+            {
+                page = 17;
+            }
+            else if (page == 14)
+            {
+                page = 99;
+            }
+            else if (page == 15)
+            {
+                page = 99;
+            }
+            else if (page == 16)
+            {
+                page = 99;
+            }
+            else if (page == 17)
+            {
+                page = 99;
+            }
+            else if (page == 18)
+            {
+                page = 99;
+            }
+            else if (page == 19)
+            {
+                page = 99;
+            }
+            displayPage();
+
+
+
+        }
+    }
+}
+
